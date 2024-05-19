e5ddaea477f611c6041cb31afbaf5e1c617adb25
Akinola <85134238+kinola-IQ@users.noreply.github.com>
2024-05-19 01:52:22 +0100

    Update Data_Analytics_projrct.py

--- Data_Analytics_projrct.py
+++ Data_Analytics_projrct.py
@@ -1,19 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Oct 13 12:48:27 2023
-
-@author: Akin
-"""
-
-
 '''
-    KEY OBJECTTIVES
-    1.) READ IN DATA FROM MULTIPLE CSV FILES
-    2.) EXPLORE THE DATA(MILLIONS OF ROWS!)
-    3.) CREATE NEW COLUMNS TO AID IN ANALYSIS
-    4.) FILTER , SORT AND AGGREGATE THE DATA TO PINPOINT AND SUMMARIZE IMPORTANT INFORMATION
-    5.) BUILD PLOTS TO COMMUNICATE KEY INSIGHTS
-
     COLUMNS TO WORK WITH:
     PROJECT_TANSACTIONS.CSV AND PRODUCT.CSV(BRIEFLY)
 '''
@@ -23,16 +8,12 @@ import seaborn as sns
 sns.set_style('darkgrid')
 
 
-''' 
-    FIRST READ IN TRANSACIONS DATA AND EXPLORE IT
-    * TAKE A LOOK AS THE RAW DATA,THE DATATYPES AND CAST DAY,QUANTITY,STORE_ID AND WEEK_NO COLUMNS
-    TO THE SMALLEST APPROPRIATE DATATYPE,CHECK THE MEMORY REDUCTION BY DOING SO
-'''
+#____________________________________________reading in neccessary data___________________________________________________________
 project_transactions= pd.read_csv('C:/Users/akinola/Documents/python DATA ANALYTICS files/Pandas Course Resources/project_data/project_transactions.csv',
                                   )
                       
                       
-this sections is to check the dtype and downcast accordingly to reduce memory usage
+#sections to check the dtype and downcast accordingly to reduce memory usage
 print(project_transactions['WEEK_NO'].max())
 
 project_transactions =(project_transactions
@@ -52,39 +33,21 @@ print(project_transactions.info(memory_usage='deep'))
 # ORIGINAL memory usage: 180.1 MB
 # NEW memory usage: 137.1 MB
 
-'''    
-    IS THERE ANY MISSING DATA?
-'''
+#checking for missing data
 print(project_transactions.isna().sum())
 #there appears to be no missing data
-'''
-    
-    * HOW MANY UNIQUE HOUSEHOLDS AND PRODUCTS ARE THERE IN THE DATA?,
-    THE FIELDS HOUSEHOLD_KEY AND PRODUCT_ID WILL HELP HERE
-'''
+
+#Checking for unique household and product values
 print(project_transactions[['household_key','PRODUCT_ID']].nunique())
 #   THE NUMBER OF UNIQUE HOUSEHOLDS AND PRODUCTS ARE:
 #household_key     2099
 #PRODUCT_ID       84138
 
-
+#_____________________________________________________________________________________________________________________
 
                       
 
- '''
-         COLUMN CREATION
-create two columns
-A column thatcaptures the total_discount by row(sum of [retail_disc],[coupon_disc])
-The percentage discount([total_discount]/[sales_value]),Make sure this is positive (try '.abs()')
-If the percentage discount is greater than 1,set it equalto 1, if it is less than 0, set it to 0.
-Drop the individualdiscount columns(retail_disc, coupon_disc , coupon_match_disc)
-feel free to overwrite the existing transaction dataframe after making the modifications above
-'''
-
-#-A column thatcaptures the total_discount by row(sum of [retail_disc]/[coupon_disc])
-#--The percentage discount([total_discount]/[sales_value]),Make sure this is positive
-#---(try '.abs()')
-#---using the .assign() method i will proceede to create two columns satisfying the above
+#______________________________________________COLUMN CREATION___________________________________________________________
 project_transactions=(project_transactions
                       .assign(
                           TOTAL_DISC = lambda x:x['RETAIL_DISC'] + x['COUPON_DISC'],
@@ -93,33 +56,34 @@ project_transactions=(project_transactions
                       )
 print(project_transactions.query("PERC_DISC in [1.0]"))
 print(project_transactions.info(memory_usage='deep'))
+#_________________________________________________________________________________________________________________________
 
 
-'''
-        OVERALL STATISTICS
-CALCULATE:
-    -THE TOTAL SALES (SUM OF SALES_VALUE)
-    -TOTAL DISCOUNT (SUM OF TOTAL_DISCOUNT)
-    -OVERALL PERCENTAGE DISCOUNT(SUM OF TOTAL_DISCOUNT/SUM OF SALES VALUE)
-    -TOTAL QUANTITY SOLD (SUM OF QUANTITY)
-    -MAX QUANTITY SOLD IN A SINGLE ROW . INSPECT THE ROW AS WELL , DOES THIS HAVE A HIGH DISCOUNT
-    PERCENTAGE?
-    -TOTAL SALES VALUE PER BASKET(SUM OF SALES_VALUE/NUNIQUE BASKET_ID)
-    -TOTAL SALES VALUE PER HOUSEHOLD(SUM OF SALES VALUE/NUNIQUE HOUSE HOLD KEY)
-'''
+#____________________________________________OVERALL STATISTICS______________________________________________________
 
+#THE TOTAL SALES (SUM OF SALES_VALUE)
 TOTAL_SALES = project_transactions['SALES_VALUE'].sum()
+
+#TOTAL DISCOUNT (SUM OF TOTAL_DISCOUNT)
 TOTAL_DISCOUNT = project_transactions['TOTAL_DISC'].sum()
+
+#OVERALL PERCENTAGE DISCOUNT(SUM OF TOTAL_DISCOUNT/SUM OF SALES VALUE)
 OVERALL_PERC_DISC = TOTAL_DISCOUNT / TOTAL_SALES
+
+#TOTAL QUANTITY SOLD (SUM OF QUANTITY)
 TOTAL_Q = project_transactions['QUANTITY'].sum()
 
+#MAX QUANTITY SOLD IN A SINGLE ROW
 MAX_Q = project_transactions['QUANTITY'].max()
 
+#checking for A HIGH DISCOUNT PERCENTAGE
 print(project_transactions.query("QUANTITY == @MAX_Q"))
 #max value does not have a high percentage discount, its per_disc == 0.0
 
+#TOTAL SALES VALUE PER BASKET(SUM OF SALES_VALUE/NUNIQUE BASKET_ID)
 TOTAL_SALES_PER_BASKET = TOTAL_SALES / project_transactions['BASKET_ID'].nunique()
 
+#TOTAL SALES VALUE PER HOUSEHOLD(SUM OF SALES VALUE/NUNIQUE HOUSE HOLD KEY)
 TOTAL_SALES_PER_HOUSEHOLD = TOTAL_SALES / project_transactions['household_key'].nunique()
 
 print(
@@ -131,13 +95,11 @@ print(
       'TOTAL_SALES_PER_BASKET =',     TOTAL_SALES_PER_BASKET,      '\n',#28.61797938516092
       'TOTAL_SALES_PER_HOUSEHOLD =',  TOTAL_SALES_PER_HOUSEHOLD         #3175.9140066698424
       )
+#__________________________________________________________________________________________________________________
 
 
- '''
-        HOUSEHOLD ANALYSIS
+ #________________________________________________________HOUSEHOLD ANALYSIS________________________________________________
 
-
-'''
 # -plot the distribution of total sales value purchased at the household level
 sales_dist = (project_transactions
               
@@ -177,27 +139,20 @@ print(top_by_SV)
 
 # -plot the total sales value for our top 10 house holds by value, order from highest to lowest
 print(top_by_SV.plot.bar())
+#_______________________________________________________________________________________________________________________
 
 
 
+#____________________________________________product analysis___________________________________________________________________
 
-'''
-                product analysis
--which product had the most sales by value?plot a horizintal bar chart
--did the top 10 seling items have a higher than average discount rate?
--what was the most common [product_id]among rows with the households in our top
-    10 households by sales value
--look up the names of the top 10 products by sales in the [product.csv]dataset
--look up the product name of  the item that had the highest quantity sold in a single row
-'''
 product = pd.read_csv('C:/Users/akinola/Documents/python DATA ANALYTICS files/Pandas Course Resources/project_data/product.csv')
-
 
 print(project_transactions.info())
 product =(product
           .assign( sales_value = project_transactions['SALES_VALUE'],
                   discount_rate = project_transactions['RETAIL_DISC']))
-# n0.1
+                  
+# which product had the most sales by value?plot a horizintal bar chart
 print(product[['COMMODITY_DESC','sales_value']]
       .nlargest(10,'sales_value')
       .sort_values(by='sales_value', ascending=False)
@@ -205,7 +160,7 @@ print(product[['COMMODITY_DESC','sales_value']]
       .plot.barh(y='sales_value', 
                   xlabel = 'commodity',
                   ylabel = 'sales value'))
-# no.2
+# did the top 10 seling items have a higher than average discount rate?
 print(product.info())
 top_10 = product[['sales_value','discount_rate']].nlargest(10,'sales_value').agg({'discount_rate':"mean",'sales_value':'mean'})
 print(top_10,'\n',
@@ -213,7 +168,7 @@ print(top_10,'\n',
       'the top 10 had a below average discount of -7.530 '
       )
 
-# no.3 
+# what was the most common [product_id]among rows with the households in our top 10 households by sales value
 print(project_transactions.info())
 #most common product id
 top_10_house =(project_transactions[['household_key','SALES_VALUE','PRODUCT_ID']].nlargest(10,'SALES_VALUE')
@@ -221,21 +176,19 @@ top_10_house =(project_transactions[['household_key','SALES_VALUE','PRODUCT_ID']
 
 print(top_10_house,'\n',"most common product_id is 1089093")
 
-# no.4
+# look up the names of the top 10 products by sales in the [product.csv]dataset
 print(product.info())
 top_10_prod_names =  (product[['COMMODITY_DESC','sales_value']]
                       .nlargest(10,'sales_value')
                       .reset_index())
 print(top_10_prod_names)
 
-# no.5
-
+# look up the product name of  the item that had the highest quantity sold in a single row
 product = product.assign(quantity = project_transactions['QUANTITY'])
 top_by_q = product.groupby("COMMODITY_DESC")[['quantity']].agg({'quantity':'sum'})
 top_by_qor = product[['COMMODITY_DESC','quantity']].nlargest(1,'quantity')
 print(top_by_qor)
-
-
+#______________________________________________________________________________________________________________
 
 
 
